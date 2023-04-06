ef30d158f3fb811f37a9fabc51709db0d0ce258f
Mouhamed Doumbia <doumbiasoft@gmail.com>
2023-04-06 15:00:38 -0400

    Update secrets_key.py

--- secrets_key.py
+++ secrets_key.py
@@ -1,3 +1,5 @@
-flask_secret_key='flask&feedback&secret&key'
+import os 
+
+flask_secret_key = os.environ.get('SECRET_KEY','flask&feedback&secret&key')
 mailing_email="noreply-apps@gmail.com"
 mailing_password="ggugcjgcjcgcjcgjgcj"
